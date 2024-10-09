41d0021c224f5197b85cb1adfc16a4c9feb0fa64
unknown <185282@BME.HU>
2024-10-09 09:48:22 +0200

    kert valtoztatasok javitasa

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -9,7 +9,7 @@ import java.util.Random;
 */
 public class TorpedoStore {
 
-  Random generator = new Random();
+  
 
   // rate of failing to fire torpedos [0.0, 1.0]
   private double FAILURE_RATE = 0.0; //NOSONAR
@@ -30,10 +30,7 @@ public class TorpedoStore {
     }
   }
 
-  void doSomething() {
-    double r = this.generator.nextDouble();
-    
-  }
+  
 
   public boolean fire(int numberOfTorpedos){
     if(numberOfTorpedos < 1 || numberOfTorpedos > this.torpedoCount){
@@ -43,12 +40,13 @@ public class TorpedoStore {
     boolean success = false;
 
     // simulate random overheating of the launcher bay which prevents firing
-    
+    Random generator = new Random();
+    double r = generator.nextDouble();
    
 
     
 
-    if (doSomething() >= FAILURE_RATE) {
+    if (r >= FAILURE_RATE) {
       // successful firing
       this.torpedoCount -= numberOfTorpedos;
       success = true;
